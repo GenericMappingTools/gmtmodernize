4380d5f74d7e9493107684bbfd4b6dabcce5075e
Leonardo Uieda <leouieda@gmail.com>
2017-06-02 10:56:15 -1000

    Update example scripts to modern mode 2.0 (#9)
    Include them in the README.
    Having conflict using jpeg from default channel.
    Removed it from environment solved the problem.

--- example/modern.sh
+++ example/modern.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Modern mode: GMT commands avoids -R -J when it can and -O -K and redirection to PS file are not user
-gmt set GMT_RUNMODE modern
+# Modern mode: GMT commands avoids -R -J (when it can) and -O -K
+# Redirection to a PS file is done in the background.
+# Commands can guess -R from the input data or previous commands.
+gmt begin
 gmt grdgradient -Nt0.2 -A45 data.nc -Gintens.nc
 gmt makecpt -Cgeo -T-8000/2000 > t.cpt
 gmt grdimage -Ct.cpt -Iintens.nc data.nc -JM6i -P
@@ -8,6 +10,6 @@ gmt pscoast -Dh -Baf -W0.75p
 echo "Japan Trench" | gmt pstext -F+f32p+cTC -Dj0/0.2i -Gwhite
 gmt psxy -W2p lines.txt
 gmt psscale -DjBL+w3i/0.1i+h+o0.3i/0.4i -Ct.cpt -W0.001 -F+gwhite+p0.5p -Bxaf -By+l"km"
-gmt psconvert -Tf -P -A -Fmy_map
-evince my_map.pdf
-rm -f intens.nc t.cpt gmt.conf gmt.history
+gmt psconvert -Tf -P -A -Fmodern_map
+gmt end
+rm -f intens.nc t.cpt
